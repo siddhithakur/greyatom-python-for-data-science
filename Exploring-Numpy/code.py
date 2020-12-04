491caa2a713690c0c0b7ad8e3fab9f5894f100ba
Siddhi Thakur <46794748+siddhithakur@users.noreply.github.com>
2020-12-04 10:31:24 +0530

    Code file created.

---
+++ Exploring-Numpy/code.py
@@ -0,0 +1,81 @@
+# --------------
+# Importing header files
+import numpy as np
+
+# Path of the file has been stored in variable called 'path'
+
+#New record
+new_record=[[50,  9,  4,  1,  0,  0, 40,  0]]
+new_record=np.array(new_record)
+
+#Code starts here
+data=np.genfromtxt(path, delimiter=",", skip_header=1)
+
+
+print(data)
+census=np.concatenate([data,new_record])
+
+
+# --------------
+#Code starts here
+age=census[:,0]
+max_age=max(age)
+min_age=min(age)
+age_mean=age.mean()
+age_std=age.std()
+
+
+# --------------
+#Code starts here
+
+#Creating new subsets based on 'Age'
+race_0=census[census[:,2]==0]
+race_1=census[census[:,2]==1]
+race_2=census[census[:,2]==2]
+race_3=census[census[:,2]==3]
+race_4=census[census[:,2]==4]
+
+
+#Finding the length of the above created subsets
+len_0=len(race_0)
+len_1=len(race_1)
+len_2=len(race_2)
+len_3=len(race_3)
+len_4=len(race_4)
+
+#Printing the length of the above created subsets
+print('Race_0: ', len_0)
+print('Race_1: ', len_1)
+print('Race_2: ', len_2)
+print('Race_3: ', len_3)
+print('Race_4: ', len_4)
+
+#Storing the different race lengths with appropriate indexes
+race_list=[len_0, len_1,len_2, len_3, len_4]
+
+#Storing the race with minimum length into a variable 
+minority_race=race_list.index(min(race_list))
+
+#Code ends here
+
+
+# --------------
+#Code starts here
+senior_citizens=census[census[:,0]>60]
+working_hours_sum=sum(senior_citizens[:,6])
+senior_citizens_len=len(senior_citizens)
+avg_working_hours=working_hours_sum/senior_citizens_len
+print(avg_working_hours)
+
+
+# --------------
+#Code starts here
+high=census[census[:,1]>10]
+low=census[census[:,1]<=10]
+
+avg_pay_high=np.mean(high[:,7])
+avg_pay_low=np.mean(low[:,7])
+
+print(avg_pay_high>avg_pay_low)
+
+
